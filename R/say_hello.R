230c81ff2b0f77e18a429f94441a2813b9e5ff2a
samanthacsik <samanthacsik@gmail.com>
2025-04-09 11:35:25 -0700

    removed a little bit for abridged version of module (but can add it back in)

--- R/say_hello.R
+++ R/say_hello.R
@@ -1,8 +1,18 @@
 say_hello <- function(name = NULL) {
-  if (is.null(name) || is.na(name) || trimws(name) == "" || is.numeric(name)) {
-    return("Please include a name for me to greet!")
+  if (!is.character(name)) {
+    stop("Please include a name as a character string for me to greet!")
   } else {
     greeting <- paste0("Hello ", name, "!")
     print(greeting)
   }
 }
+
+# # Improvement ----
+# say_hello <- function(name = NULL) {
+#   if (!is.character(name) || trimws(name) == "") {
+#     stop("Please include a name as a character string for me to greet!")
+#   } else {
+#     greeting <- paste0("Hello ", name, "!")
+#     print(greeting)
+#   }
+# }
--- tests/test_say_hello.R
+++ tests/test_say_hello.R
@@ -5,30 +5,23 @@ library(testthat)
 source(here::here("R", "say_hello.R"))
 
 #..............................tests.............................
-test_that("say_hello works with a string", {
-  expect_output(say_hello("Sam"), "Hello Sam!")
+test_that("say_hello works with a string", { # give your test a descriptive name
+  expect_output(say_hello("Sam"), "Hello Sam!") # write an expectation (i.e. expected output given a known input)
 })
 
-test_that("say_hello prints message when no argument is provided", {
-  expect_equal(say_hello(), "Please include a name for me to greet!")
+test_that("say_hello throws error when no argument is provided", {
+  expect_error(say_hello(), "Please include a name as a character string for me to greet!")
 })
 
-test_that("say_hello prints message when NA is provided", {
-  expect_equal(say_hello(NA), "Please include a name for me to greet!")
+test_that("say_hello throws error when numeric value is provided", {
+  expect_error(say_hello(123), "Please include a name as a character string for me to greet!")
 })
 
-test_that("say_hello prints message when empty string is provided", {
-  expect_equal(say_hello(""), "Please include a name for me to greet!")
-})
-
-test_that("say_hello prints message when empty string with white space is provided", {
-  expect_equal(say_hello("  "), "Please include a name for me to greet!")
-})
-
-test_that("say_hello prints message when integer is provided", {
-  expect_equal(say_hello(123), "Please include a name for me to greet!")
-})
-
-test_that("say_hello prints message when decimal is provided", {
-  expect_equal(say_hello(1.23), "Please include a name for me to greet!")
-})
+# # Added after improving function ----
+# test_that("say_hello throws error when an empty string is provided", {
+#   expect_error(say_hello(""), "Please include a name as a character string for me to greet!")
+# })
+# 
+# test_that("say_hello throws error when empty string with white space is provided", {
+#   expect_error(say_hello(" "), "Please include a name as a character string for me to greet!")
+# })

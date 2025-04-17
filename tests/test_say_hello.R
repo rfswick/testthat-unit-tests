18cdfa0458049b366b2751f66893ac458460559b
samanthacsik <samanthacsik@gmail.com>
2025-04-16 19:06:21 -0700

    added example of all assertions inside test_stat() (not best practice)

--- tests/test_say_hello.R
+++ tests/test_say_hello.R
@@ -9,13 +9,21 @@ test_that("say_hello works with a string", { # give your test a descriptive name
   expect_output(say_hello("Sam"), "Hello Sam!") # write an expectation (i.e. expected output given a known input)
 })
 
+test_that("say_hello throws error when numeric value is provided", {
+  expect_error(say_hello(123), "Please include a name as a character string for me to greet!")
+})
+
 test_that("say_hello throws error when no argument is provided", {
   expect_error(say_hello(), "Please include a name as a character string for me to greet!")
 })
 
-test_that("say_hello throws error when numeric value is provided", {
-  expect_error(say_hello(123), "Please include a name as a character string for me to greet!")
-})
+# # ----- same as above, but not best practice; keep tests small ----
+# test_that("testing say_hello()", {
+#   expect_output(say_hello("Sam"), "Hello Sam!")
+#   expect_error(say_hello(), "Please include a name as a character string for me to greet!")
+#   expect_error(say_hello(123), "Please include a name as a character string for me to greet!")
+# })
+# # -----------------------------------------------------------------
 
 # # Added after improving function ----
 # test_that("say_hello throws error when an empty string is provided", {
